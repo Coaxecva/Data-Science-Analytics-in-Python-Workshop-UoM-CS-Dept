707460619ec24f7de0f92173b7102cba00e4d4b8
Quang Tran <tmquang@gmail.com>
2017-12-09 09:35:59 -0600

    Update movies.py

--- movies.py
+++ movies.py
@@ -5,8 +5,7 @@ u = pandas.read_csv('../data/movielens/users.csv')
 m = pandas.read_csv('../data/movielens/movies.csv')
 r = pandas.read_csv('../data/movielens/ratings.csv')
 
-ur = pandas.merge(u,r)
-data = pandas.merge(m, ur)
+data = pandas.merge(m, pandas.merge(u,r))
 
 # What are the age groups?
 data['age'].value_counts()
@@ -17,3 +16,16 @@ data['gender'].value_counts()
 # Show histogram of 50 random samples
 data['age'].sample(50).plot.hist()
 # pyplot.show()
+
+# Select entries from the 1-34 age group.
+df = data[ data.age <= 18 ]
+
+# Select entries reviewers under 18 and above 50
+df = data[ (data.age <= 18) | (data.age >= 50) ]
+
+# What are the most popular movies?  GroupBy, Aggregate
+by_titles = data.groupby('title')
+by_titles_count = by_titles.count()
+sorted = by_titles_count.sort_values(by='rating', ascending=False)
+
+by_titles_mean = by_titles.mean()
