105158c471b7e4d949ecd0a7d2f9410fba685987
Quang Tran <tmquang@gmail.com>
2017-12-10 13:53:11 -0600

    Create decision_tree_precision_recall.py

---
+++ decision_tree_precision_recall.py
@@ -0,0 +1,43 @@
+import pandas
+from sklearn.model_selection import train_test_split, ShuffleSplit, cross_validate, cross_val_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
+import lcplot
+from matplotlib import pyplot
+
+#------------------------------------------------------
+def validate(model, X, y):
+	shuffle100 = ShuffleSplit(n_splits=100, test_size=0.1)
+	scores = cross_validate(model, X, y, cv=shuffle100, scoring=['precision_weighted', 'recall_weighted', 'accuracy'])
+	print('Precision:', round(scores['test_precision_weighted'].mean(),2))
+	print('Recall:   ', round(scores['test_recall_weighted'].mean(),2))
+	print('Accuracy: ', round(scores['test_accuracy'].mean(),2))
+
+#------------------------------------------------------
+
+data = pandas.read_csv('../data/iris.csv')
+
+# y : Species
+# X : everything else
+# model : decision tree
+y = data['Species']
+X = data.drop('Species', axis=1)
+
+# Split data into training and testing sets.
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
+# Build a decision model using training data.
+
+# Find out good training size
+# lcplot.plot(DecisionTreeClassifier(), X, y)
+# we'll use 0.1 for test_size
+
+# ----------
+dt = DecisionTreeClassifier()
+dt.fit(X_train, y_train)
+
+# Test your model using the testing data.
+scores = dt.score(X_test, y_test)
+
+# Validate
+validate(dt, X,  y)
+
