6c51849cd75a790f9956f6681f3f855729e32ff9
Quang Tran <tmquang@gmail.com>
2017-12-10 11:07:25 -0600

    Update cali_housing.py

--- cali_housing.py
+++ cali_housing.py
@@ -1,7 +1,18 @@
 import pandas
 from matplotlib import pyplot
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split, cross_val_score, ShuffleSplit
+
 data_file = '../data/cali_housing.csv'
 data = pandas.read_csv(data_file)
+
+#------------------------------------------------------------------------
+def evaluate(model, X, y):
+	print(model)
+	scores = cross_val_score(model, X, y, cv=ShuffleSplit(50, train_size=0.9))
+	print('R:', scores.mean())
+
+#------------------------------------------------------------------------
 
 data.info()
 data.sum()
